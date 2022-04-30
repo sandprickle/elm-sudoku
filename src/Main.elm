cc6c67e21ff5d71bd06eeeca8680c5cc4058fa5b
Bryce Miller <bryce@brycem.co>
2022-04-30 18:36:51 -0400

    add keyboard navigation;

--- src/Main.elm
+++ src/Main.elm
@@ -63,7 +63,7 @@ initialModel =
 type Msg
     = ClickedCell { x : Int, y : Int }
     | ClickedCheckPuzzle
-    | KeyUp RawKey
+    | KeyDown RawKey
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -83,40 +83,116 @@ update msg model =
             in
             ( { model | puzzleStatus = puzzleStatus }, Cmd.none )
 
-        KeyUp key ->
-            let
-                keyStr : String
-                keyStr =
-                    case Keyboard.characterKeyOriginal key of
-                        Just (Keyboard.Character str) ->
-                            str
+        KeyDown key ->
+            handleKeyInput key model
 
-                        _ ->
-                            ""
-            in
-            if List.member keyStr valueKeys then
-                case model.selectedCell of
-                    Just coord ->
-                        ( { model
-                            | currentPuzzle =
-                                Grid.setByCoord
-                                    coord
-                                    model.currentPuzzle
-                                    (Cell.fromString keyStr)
-                          }
+
+handleKeyInput : RawKey -> Model -> ( Model, Cmd Msg )
+handleKeyInput key model =
+    let
+        keyStr : String
+        keyStr =
+            case Keyboard.characterKeyOriginal key of
+                Just (Keyboard.Character str) ->
+                    str
+
+                _ ->
+                    ""
+    in
+    if List.member keyStr valueKeys then
+        case model.selectedCell of
+            Just coord ->
+                ( { model
+                    | currentPuzzle =
+                        Grid.setByCoord
+                            coord
+                            model.currentPuzzle
+                            (Cell.fromString keyStr)
+                  }
+                , Cmd.none
+                )
+
+            Nothing ->
+                ( model, Cmd.none )
+
+    else if List.member keyStr navKeys then
+        case model.selectedCell of
+            Nothing ->
+                ( { model | selectedCell = Just { x = 4, y = 4 } }, Cmd.none )
+
+            Just coord ->
+                case keyStr of
+                    "h" ->
+                        ( { model | selectedCell = Just (moveSelectionLeft coord) }
                         , Cmd.none
                         )
 
-                    Nothing ->
+                    "j" ->
+                        ( { model | selectedCell = Just (moveSelectionDown coord) }
+                        , Cmd.none
+                        )
+
+                    "k" ->
+                        ( { model | selectedCell = Just (moveSelectionUp coord) }
+                        , Cmd.none
+                        )
+
+                    "l" ->
+                        ( { model | selectedCell = Just (moveSelectionRight coord) }
+                        , Cmd.none
+                        )
+
+                    _ ->
                         ( model, Cmd.none )
 
-            else
-                ( model, Cmd.none )
+    else
+        ( model, Cmd.none )
 
 
 valueKeys : List String
 valueKeys =
     [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
+
+navKeys : List String
+navKeys =
+    [ "h", "j", "k", "l" ]
+
+
+moveSelectionLeft : Coord -> Coord
+moveSelectionLeft { x, y } =
+    if y == 0 then
+        { x = x, y = 8 }
+
+    else
+        { x = x, y = y - 1 }
+
+
+moveSelectionRight : Coord -> Coord
+moveSelectionRight { x, y } =
+    if y == 8 then
+        { x = x, y = 0 }
+
+    else
+        { x = x, y = y + 1 }
+
+
+moveSelectionUp : Coord -> Coord
+moveSelectionUp { x, y } =
+    if x == 0 then
+        { x = 8, y = y }
+
+    else
+        { x = x - 1, y = y }
+
+
+moveSelectionDown : Coord -> Coord
+moveSelectionDown { x, y } =
+    if x == 8 then
+        { x = 0, y = y }
+
+    else
+        { x = x + 1, y = y }
 
 
 
@@ -189,4 +265,4 @@ viewPuzzle selectedCell puzzle =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Keyboard.ups KeyUp
+    Keyboard.downs KeyDown
